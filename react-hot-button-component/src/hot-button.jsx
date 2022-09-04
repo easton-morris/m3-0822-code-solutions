85ec33d0a37445577f8150e34e1edcbe612f9f1b
Easton Morris <eastonmorris9@gmail.com>
2022-09-04 07:21:00 +0000

    fixed call in props and adjusted handlers

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -10,48 +10,49 @@ export default class HotButton extends React.Component {
     };
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleHeat = this.handleHeat.bind(this);
   }
 
   handleClick() {
     let currHeat = this.state.heat;
+    currHeat++;
+    this.handleHeat(currHeat);
     this.setState({
-      heat: currHeat++
+      heat: currHeat
     });
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState !== this.state) {
-      if (prevState.heat === 3) {
-        this.setState({
-          color: 'three-heat'
-        });
-      } else if (prevState.heat === 6) {
-        this.setState({
-          color: 'six-heat'
-        });
-      } else if (prevState.heat === 9) {
-        this.setState({
-          color: 'nine-heat'
-        });
-      } else if (prevState.heat === 12) {
-        this.setState({
-          color: 'twelve-heat'
-        });
-      } else if (prevState.heat === 15) {
-        this.setState({
-          color: 'fifteen-heat'
-        });
-      } else if (prevState.heat === 18) {
-        this.setState({
-          color: 'eighteen-heat'
-        });
-      }
+  handleHeat(currHeat) {
+    if (currHeat === 3) {
+      this.setState({
+        color: 'three-heat'
+      });
+    } else if (currHeat === 6) {
+      this.setState({
+        color: 'six-heat'
+      });
+    } else if (currHeat === 9) {
+      this.setState({
+        color: 'nine-heat'
+      });
+    } else if (currHeat === 12) {
+      this.setState({
+        color: 'twelve-heat'
+      });
+    } else if (currHeat === 15) {
+      this.setState({
+        color: 'fifteen-heat'
+      });
+    } else if (currHeat === 18) {
+      this.setState({
+        color: 'eighteen-heat'
+      });
     }
   }
 
   render() {
     return (
-      <button onClick={this.handleClick()} className={this.state.color}>Hot Button</button>
+      <button onClick={this.handleClick} className={this.state.color}>Hot Button</button>
     );
   }
 }
